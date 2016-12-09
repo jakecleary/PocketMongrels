a689aea06248c59696f1ae1467e44e76822f1592
Jake Cleary <jake.cleary@mediatonic.co.uk>
2016-12-09 13:22:25 +0000

    Add new Services project
    This will house the bulk of any business logic surrounding entities.

---
+++ src/JakeCleary.PocketMongrels.Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using JakeCleary.PocketMongrels.Core;
+using JakeCleary.PocketMongrels.Data;
+
+namespace JakeCleary.PocketMongrels.Services
+{
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public List<User> All()
+        {
+            return _userRepository.All();
+        }
+
+        public User FindById(Guid id)
+        {
+            return _userRepository.ByGuid(id);
+        }
+
+        public bool Add(User user)
+        {
+            _userRepository.Add(user);
+
+            return true;
+        }
+
+        public bool Remove(User user)
+        {
+            _userRepository.Remove(user);
+
+            return true;
+        }
+    }
+}
