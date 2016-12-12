cef6cecbde8ba99aa211e9bc033e64cd7273103c
Jake Cleary <jake.cleary@mediatonic.co.uk>
2016-12-12 17:03:53 +0000

    Add a test to ensure we're validating the animal's Type

---
+++ src/JakeCleary.PocketMongrels.Tests/GivenIWantANewPet/WhenISpecifyAnInvalidType.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using JakeCleary.PocketMongrels.Api.Resourses;
+using NUnit.Framework;
+
+namespace JakeCleary.PocketMongrels.Tests.GivenIWantANewPet
+{
+    class WhenISpecifyAnInvalidType
+    {
+        private FakeServer _server;
+        private ApiResponse<Animal> _response;
+        private Guid _ownerId;
+
+        [TestFixtureSetUp]
+        public void FixtureInit()
+        {
+            _server = new FakeServer();
+
+            var userResponse = _server
+                .NewRequestTo("/api/users/")
+                .Post<User>("{'Name': 'Jake'}");
+
+            _ownerId = userResponse.Resource.Id;
+
+            _response = _server
+                .NewRequestTo($"/api/users/{_ownerId}/animals")
+                .Post<Animal>("{'Name': 'Snuffles the Rabbit', 'Type': 99}");
+        }
+
+        [Test]
+        public void ThenTheRequestIsUnsuccessful()
+        {
+            Assert.That(_response.Success, Is.False);
+        }
+
+        [Test]
+        public void ThenTheResponseCodeIsAppropriate()
+        {
+            Assert.That(_response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        }
+    }
+}
