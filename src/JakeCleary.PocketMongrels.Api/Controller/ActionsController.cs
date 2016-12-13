e5bc6fda8341050d9e1e179f566a0bad57eb02b7
Jake Cleary <jake.cleary@mediatonic.co.uk>
2016-12-13 12:39:41 +0000

    Clean up the ActionsController
    - Swap out the IUserRepository for the UserService.
    - Use the short-hand response methods until a more manageable response
      formatting method is found.
    - Only return IHttpActionResults.

--- src/JakeCleary.PocketMongrels.Api/Controller/ActionsController.cs
+++ src/JakeCleary.PocketMongrels.Api/Controller/ActionsController.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using JakeCleary.PocketMongrels.Core;
-using JakeCleary.PocketMongrels.Data;
 using JakeCleary.PocketMongrels.Services;
 
 namespace JakeCleary.PocketMongrels.Api.Controller
@@ -12,12 +8,12 @@ namespace JakeCleary.PocketMongrels.Api.Controller
     [RoutePrefix("api/users/{userId:guid}/animals/{animalId:guid}")]
     public class ActionsController : ApiController
     {
-        private readonly IUserRepository _userRepository;
+        private readonly UserService _userService;
         private readonly AnimalService _animalService;
 
-        public ActionsController(IUserRepository userRepository, AnimalService animalService)
+        public ActionsController(UserService userService, AnimalService animalService)
         {
-            _userRepository = userRepository;
+            _userService = userService;
             _animalService = animalService;
         }
         
@@ -25,80 +21,38 @@ namespace JakeCleary.PocketMongrels.Api.Controller
         [Route("feed")]
         public IHttpActionResult Feed([FromUri] Guid userId, [FromUri] Guid animalId)
         {
-            // Get the animal.
-            var user = _userRepository.ByGuid(userId);
+            var user = _userService.FindById(userId);
 
             if (user == null)
-            {
-                // Send a 404 response if the user doesn't exist.
-                var response = new HttpResponseMessage(HttpStatusCode.NotFound)
-                {
-                    Content = new StringContent("User not found.")
-                };
-
-                throw new HttpResponseException(response);
-            }
+                return NotFound();
 
             var animal = user.Animals.FirstOrDefault(a => a.Id == animalId);
 
             if (animal == null)
-            {
-                // Send a 404 response if the animal doesn't exist.
-                var response = new HttpResponseMessage(HttpStatusCode.NotFound)
-                {
-                    Content = new StringContent("Animal not found.")
-                };
-
-                throw new HttpResponseException(response);
-            }
+                return NotFound();
 
             _animalService.Feed(animal);
             
-            return Ok(animal);
-
-            // Return the full animal object back.
-            //var successResponse = Request.CreateResponse(HttpStatusCode.OK, );
-            //successResponse.Headers.Location = new Uri($"http://localhost:53460/api/users/{userId}/animals/{animalId}");
-            //return successResponse;
+            return Ok();
         }
 
         [HttpPost]
         [Route("pet")]
-        public HttpResponseMessage Pet([FromUri] Guid userId, [FromUri] Guid animalId)
+        public IHttpActionResult Pet([FromUri] Guid userId, [FromUri] Guid animalId)
         {
-            // Get the animal.
-            var user = _userRepository.ByGuid(userId);
+            var user = _userService.FindById(userId);
 
             if (user == null)
-            {
-                // Send a 404 response if the user doesn't exist.
-                var response = new HttpResponseMessage(HttpStatusCode.NotFound)
-                {
-                    Content = new StringContent("User not found.")
-                };
-
-                throw new HttpResponseException(response);
-            }
+                return NotFound();
 
             var animal = user.Animals.FirstOrDefault(a => a.Id == animalId);
 
             if (animal == null)
-            {
-                // Send a 404 response if the animal doesn't exist.
-                var response = new HttpResponseMessage(HttpStatusCode.NotFound)
-                {
-                    Content = new StringContent("Animal not found.")
-                };
-
-                throw new HttpResponseException(response);
-            }
+                return NotFound();
 
             _animalService.Pet(animal);
 
-            // Return the full animal object back.
-            var successResponse = Request.CreateResponse(HttpStatusCode.OK, animal);
-            successResponse.Headers.Location = new Uri($"http://localhost:53460/api/users/{userId}/animals/{animalId}");
-            return successResponse;
+            return Ok();
         }
     }
 }
